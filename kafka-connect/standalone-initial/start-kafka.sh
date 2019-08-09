b69fa0b957ebc8f3089c2b97e79b3e4c7c43193e
Marcos Maia <maia.marcos@gmail.com>
2019-08-09 11:26:47 +0200

    removed shebang and added some better comments to the properties files

--- kafka-connect/standalone-initial/start-kafka.sh
+++ kafka-connect/standalone-initial/start-kafka.sh
@@ -1,4 +1,2 @@
-#!/bin/bash -e
-
 # connector start command here.
 exec "/opt/kafka/bin/connect-standalone.sh" "/opt/kafka/config/connect-standalone.properties" "/opt/kafka/config/connect-file-source.properties"
